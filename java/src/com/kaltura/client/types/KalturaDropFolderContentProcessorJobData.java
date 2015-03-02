09df24d8be6a7a3cb4ee8bc71beb2f6637d2b5bd
root <root@ce-front0.dev.kaltura.com>
2015-03-02 00:26:39 -0500

    New clientlib ver

--- java/src/com/kaltura/client/types/KalturaDropFolderContentProcessorJobData.java
+++ java/src/com/kaltura/client/types/KalturaDropFolderContentProcessorJobData.java
@@ -39,7 +39,7 @@ import org.w3c.dom.NodeList;
 /**
  * This class was generated using generate.php
  * against an XML schema provided by Kaltura.
- * @date Sun, 01 Mar 15 00:21:56 -0500
+ * @date Mon, 02 Mar 15 00:21:18 -0500
  * 
  * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
  */
