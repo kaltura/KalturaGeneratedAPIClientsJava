e64e70d2de2c6e5d86de9c95b7fb3f3456ff58cc
root <root@ce-front0.dev.kaltura.com>
2015-03-07 12:22:21 -0500

    New clientlib ver

--- java/src/com/kaltura/client/services/KalturaLiveStreamService.java
+++ java/src/com/kaltura/client/services/KalturaLiveStreamService.java
@@ -44,7 +44,7 @@ import com.kaltura.client.KalturaFile;
 /**
  * This class was generated using generate.php
  * against an XML schema provided by Kaltura.
- * @date Sat, 07 Mar 15 11:28:10 -0500
+ * @date Sat, 07 Mar 15 12:17:54 -0500
  * 
  * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
  */
--- java/src/com/kaltura/client/test/KalturaTestConfig.java
+++ java/src/com/kaltura/client/test/KalturaTestConfig.java
@@ -28,10 +28,10 @@
 package com.kaltura.client.test;
 
 public class KalturaTestConfig {
-	public static final int PARTNER_ID = 101;
-	public static final String SECRET = "9c9f8d1cbb6bab63f3ac8450fcdb33f7";
-	public static final String ADMIN_SECRET = "e068809c752fb5f311217eb3da3bc74d";
-	public static final String ENDPOINT = "http://54.159.220.35";
+	public static final int PARTNER_ID = 867352;
+	public static final String SECRET = "3c289542d6a5b6a064388e2eb755be2a";
+	public static final String ADMIN_SECRET = "e842756d94dacf813e04db720fee9119";
+	public static final String ENDPOINT = "http://www.kaltura.com";
 	public static final String TEST_URL = "http://www.kaltura.org/demos/videos/DemoVideo.flv";
 	public static final String UPLOAD_VIDEO = "DemoVideo.flv";
 	public static final String UPLOAD_IMAGE = "DemoImage.jpg";
