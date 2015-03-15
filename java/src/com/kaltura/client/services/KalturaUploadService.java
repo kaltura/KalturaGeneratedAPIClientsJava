e8b075486c8d03cf4f357062ac3f33b32f378060
root <root@ce-front0.dev.kaltura.com>
2015-03-15 10:11:10 -0400

    New clientlib ver

--- java/src/com/kaltura/client/services/KalturaUploadService.java
+++ java/src/com/kaltura/client/services/KalturaUploadService.java
@@ -43,7 +43,7 @@ import com.kaltura.client.types.*;
 /**
  * This class was generated using generate.php
  * against an XML schema provided by Kaltura.
- * @date Sun, 15 Mar 15 09:40:35 -0400
+ * @date Sun, 15 Mar 15 10:07:47 -0400
  * 
  * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
  */
