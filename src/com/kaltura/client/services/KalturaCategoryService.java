445694d5ca91254673739e0843d2599c2227aa30
Kaltura CSI <community@kaltura.com>
2015-03-22 13:42:46 -0400

    New clientlib ver

--- src/com/kaltura/client/services/KalturaCategoryService.java
+++ src/com/kaltura/client/services/KalturaCategoryService.java
@@ -44,7 +44,7 @@ import com.kaltura.client.KalturaFile;
 /**
  * This class was generated using generate.php
  * against an XML schema provided by Kaltura.
- * @date Sun, 22 Mar 15 09:46:06 -0400
+ * @date Sun, 22 Mar 15 13:39:06 -0400
  * 
  * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
  */
--- src/com/kaltura/client/services/KalturaSchemaService.java
+++ src/com/kaltura/client/services/KalturaSchemaService.java
@@ -36,7 +36,7 @@ import com.kaltura.client.KalturaApiException;
 /**
  * This class was generated using generate.php
  * against an XML schema provided by Kaltura.
- * @date Sun, 22 Mar 15 09:46:06 -0400
+ * @date Sun, 22 Mar 15 13:39:06 -0400
  * 
  * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
  */
