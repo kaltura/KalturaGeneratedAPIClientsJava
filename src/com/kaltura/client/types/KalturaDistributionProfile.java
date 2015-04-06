666f6600e25e1eb862a55d94fc328cca5250a62a
Kaltura CSI <community@kaltura.com>
2015-04-06 00:24:42 -0400

    New clientlib ver

--- src/com/kaltura/client/types/KalturaDistributionProfile.java
+++ src/com/kaltura/client/types/KalturaDistributionProfile.java
@@ -43,7 +43,7 @@ import org.w3c.dom.NodeList;
 /**
  * This class was generated using generate.php
  * against an XML schema provided by Kaltura.
- * @date Sun, 05 Apr 15 12:30:55 -0400
+ * @date Mon, 06 Apr 15 00:20:52 -0400
  * 
  * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
  */
