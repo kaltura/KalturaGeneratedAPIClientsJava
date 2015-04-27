92d8c8de9859125edb3bc86c36afc6de592077c9
Kaltura CSI <community@kaltura.com>
2015-04-27 00:26:35 -0400

    New clientlib ver

--- src/com/kaltura/client/services/KalturaSchemaService.java
+++ src/com/kaltura/client/services/KalturaSchemaService.java
@@ -36,7 +36,7 @@ import com.kaltura.client.KalturaApiException;
 /**
  * This class was generated using generate.php
  * against an XML schema provided by Kaltura.
- * @date Sun, 26 Apr 15 00:10:16 -0400
+ * @date Mon, 27 Apr 15 00:22:58 -0400
  * 
  * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
  */
