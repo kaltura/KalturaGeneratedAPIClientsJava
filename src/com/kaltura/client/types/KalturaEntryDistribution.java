d35f1d38dd60d3745639aaeb518a413b8c7104a1
Kaltura CSI <community@kaltura.com>
2015-05-11 14:14:11 -0400

    New clientlib ver

--- src/com/kaltura/client/types/KalturaEntryDistribution.java
+++ src/com/kaltura/client/types/KalturaEntryDistribution.java
@@ -45,7 +45,7 @@ import org.w3c.dom.NodeList;
 /**
  * This class was generated using generate.php
  * against an XML schema provided by Kaltura.
- * @date Mon, 11 May 15 00:20:09 -0400
+ * @date Mon, 11 May 15 14:10:07 -0400
  * 
  * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
  */
