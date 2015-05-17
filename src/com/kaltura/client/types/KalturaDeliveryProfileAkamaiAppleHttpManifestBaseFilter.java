44a21020451e8c05acb9e2808df4f94d89e525d2
Kaltura CSI <community@kaltura.com>
2015-05-17 10:19:29 -0400

    New clientlib ver

--- src/com/kaltura/client/types/KalturaDeliveryProfileAkamaiAppleHttpManifestBaseFilter.java
+++ src/com/kaltura/client/types/KalturaDeliveryProfileAkamaiAppleHttpManifestBaseFilter.java
@@ -35,7 +35,7 @@ import com.kaltura.client.KalturaApiException;
 /**
  * This class was generated using generate.php
  * against an XML schema provided by Kaltura.
- * @date Sun, 17 May 15 00:21:19 -0400
+ * @date Sun, 17 May 15 10:15:18 -0400
  * 
  * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
  */
