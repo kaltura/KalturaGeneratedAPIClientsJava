272c38e6d9728e8cd3b071c31c5c3b87c78ecccc
Kaltura CSI <community@kaltura.com>
2015-07-19 01:05:29 -0400

    New clientlib ver

--- src/com/kaltura/client/enums/KalturaShortLinkStatus.java
+++ src/com/kaltura/client/enums/KalturaShortLinkStatus.java
@@ -30,7 +30,7 @@ package com.kaltura.client.enums;
 /**
  * This class was generated using generate.php
  * against an XML schema provided by Kaltura.
- * @date Sun, 19 Jul 15 00:27:42 -0400
+ * @date Sun, 19 Jul 15 01:01:25 -0400
  * 
  * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
  */
--- src/com/kaltura/client/services/KalturaSystemService.java
+++ src/com/kaltura/client/services/KalturaSystemService.java
@@ -38,7 +38,7 @@ import com.kaltura.client.types.*;
 /**
  * This class was generated using generate.php
  * against an XML schema provided by Kaltura.
- * @date Sun, 19 Jul 15 00:27:42 -0400
+ * @date Sun, 19 Jul 15 01:01:26 -0400
  * 
  * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
  */
