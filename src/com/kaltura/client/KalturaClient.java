a594230cef360e031f19c29b9597d90d3444902b
Kaltura CSI <community@kaltura.com>
2015-07-22 00:38:35 -0400

    New clientlib ver

--- src/com/kaltura/client/KalturaClient.java
+++ src/com/kaltura/client/KalturaClient.java
@@ -117,7 +117,7 @@ import com.kaltura.client.types.KalturaBaseResponseProfile;
 /**
  * This class was generated using generate.php
  * against an XML schema provided by Kaltura.
- * @date Tue, 21 Jul 15 00:32:47 -0400
+ * @date Wed, 22 Jul 15 00:34:41 -0400
  * 
  * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
  */
@@ -128,7 +128,7 @@ public class KalturaClient extends KalturaClientBase {
 	public KalturaClient(KalturaConfiguration config) {
 		super(config);
 		
-		this.setClientTag("java:15-07-21");
+		this.setClientTag("java:15-07-22");
 		this.setApiVersion("3.2.0");
 	}
 	
--- src/com/kaltura/client/services/KalturaUploadTokenService.java
+++ src/com/kaltura/client/services/KalturaUploadTokenService.java
@@ -43,7 +43,7 @@ import com.kaltura.client.KalturaFile;
 /**
  * This class was generated using generate.php
  * against an XML schema provided by Kaltura.
- * @date Tue, 21 Jul 15 00:32:47 -0400
+ * @date Wed, 22 Jul 15 00:34:41 -0400
  * 
  * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
  */
