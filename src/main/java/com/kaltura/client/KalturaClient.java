1889d56652c7f672d13478e028a5f9c71217ecbf
Kaltura CSI <community@kaltura.com>
2015-10-19 00:52:06 -0400

    New clientlib ver

--- src/main/java/com/kaltura/client/KalturaClient.java
+++ src/main/java/com/kaltura/client/KalturaClient.java
@@ -128,7 +128,7 @@ public class KalturaClient extends KalturaClientBase {
 	public KalturaClient(KalturaConfiguration config) {
 		super(config);
 		
-		this.setClientTag("java:15-10-18");
+		this.setClientTag("java:15-10-19");
 		this.setApiVersion("3.3.0");
 	}
 	
