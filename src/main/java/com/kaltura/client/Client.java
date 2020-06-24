eab7deb06f94cfb6aec344c0e6ab48a8c971403c
Kaltura CSI <community@kaltura.com>
2020-06-23 20:41:14 -0400

    New clientlib ver

--- src/main/java/com/kaltura/client/Client.java
+++ src/main/java/com/kaltura/client/Client.java
@@ -43,7 +43,7 @@ public class Client extends ClientBase {
 	public Client(ConnectionConfiguration config) {
 		super(config);
 		
-		this.setClientTag("java:20-06-22");
+		this.setClientTag("java:20-06-23");
 		this.setApiVersion("16.5.0");
 		this.clientConfiguration.put("format", 1); // JSON
 	}
